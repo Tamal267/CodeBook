e2e1b14e4c568f0ae83a6ed2f9f7691bc87be0c2
Tamal267 <syedtamal@gmail.com>
2024-10-26 18:45:50 +0600

    sos up

--- code/Dynamic Programming/SOS.cc
+++ code/Dynamic Programming/SOS.cc
@@ -3,19 +3,24 @@ void SOS (vector<int> &v) {
 	vector<int> freq(1 << BITS, 0);
 	for (int mask : v) freq[mask]++;
 
-	vector<int> dp(1 << BITS);
+	vector<vector<int>> dp(BITS + 1, vector<int> (1 << BITS, 0));
 	for (int mask = 0; mask < 1 << BITS; mask++) {
-		dp[mask] = 0;
-		int submask = mask;
-		do {
-			dp[mask] += freq[submask];
-			submask = (submask - 1) & mask;
-		} while (submask);
+		dp[0][mask] = freq[mask];
 	}
 
-	for (int mask : v) cout << dp[mask] << '\n';
+	for (int bits = 1; bits <= BITS; bits++) {
+		for (int mask = 0; mask < 1 << BITS; mask++) {
+			if ((mask & (1 << (bits - 1))) == 0) {
+				dp[bits][mask] = dp[bits - 1][mask];
+			}
+			else {
+				int other_mask = mask - (1 << (bits - 1));
+				dp[bits][mask] = dp[bits - 1][mask] + dp[bits - 1][other_mask];
+			}
+		}
+	}
+
+	for (int mask : v) cout << dp[BITS][mask] << '\n';
 }
-// If k = number of 1's in the integer n. Then total submask = 2^k.
-// 5
-// 3 7 2 9 2
-// for 3 -> 3, 2, 2 are submasks.
+// dp[bits][mask] means left most 'bits' of submasks can be differ
+// for dp[1][11] 01, 00 are now allow because leftmost 1 bit can be differ. 10 and 11 are allowed.
