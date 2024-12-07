4522d4bc6d1b0aa0c63ba56ea182956a7ed81cfd
Tamal267 <syedtamal@gmail.com>
2024-12-07 09:33:01 +0600

    sieve code correction

--- code/Number Theory And Maths/Sieve.cc
+++ code/Number Theory And Maths/Sieve.cc
@@ -4,7 +4,7 @@ int notprime[N];
 
 void sieve() {
   primes.push_back(2);
-  for (int i = 2; i < N; i += 2) {
+  for (int i = 4; i < N; i += 2) {
     notprime[i] = true;
   }
   for (int i = 3; i < N; i += 2) {
