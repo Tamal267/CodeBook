6c58bccd6b8fca4dbae5c4e8d7ac11b17fc89034
Tamal267 <syedtamal@gmail.com>
2024-12-14 11:59:28 +0600

    ordered set modified

--- code/Data Structures/OrderedSet.cc
+++ code/Data Structures/OrderedSet.cc
@@ -1,21 +1,8 @@
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds; 
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 
-ordered_set o_set;
-
-// insert function to insert in
-// ordered set same as SET STL
-o_set.insert(5);
-o_set.insert(1);
-o_set.insert(2);
-
-// Finding the second smallest element
-// in the set using * because
-//  find_by_order returns an iterator
-*(o_set.find_by_order(1))
-
-// Finding the number of elements
-// strictly less than k=4
-o_set.order_of_key(4)
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// o_set<int> st;
+// *(st.find_by_order(index))
+// st.order_of_key(value)
